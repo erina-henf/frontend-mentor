4345bebfdf4b659c7ced33726a690b3600b07a2b
erina-henf <erinahenfridsen@gmail.com>
2024-10-13 17:24:53 +0200

    style commit

---
+++ product-list-with-cart-main/js/index.js
@@ -0,0 +1,6 @@
+/* VALUTA
+1. API-nyckel: Se till att byta ut 'DIN_API_NYCKEL' med din faktiska API-nyckel.
+2. Valuta: Du kan ändra baseCurrency till den valuta du vill ha som bas.
+3. Felhantering: Se till att inkludera felhantering för att fånga eventuella problem med API-anropet.
+4. Uppdatering i realtid: Du kan köra funktionen med jämna mellanrum för att uppdatera valutakurserna i realtid (t.ex. med setInterval).
+*/
